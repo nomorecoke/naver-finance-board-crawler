ed86822cc7cf93888ef332ffbe8665fb91fd8eb0
gyusu han <slhopy@gmail.com>
2018-10-03 17:22:57 +0900

    Change dataframe index to nid
    Before: indexing with date
    After: indexing with nid

--- crawler.py
+++ crawler.py
@@ -95,13 +95,12 @@ class Crawler:
             # list of dict -> dict of list
             posts = {k: [dic[k] for dic in posts] for k in posts[0]}
 
-            latest_date = self.db.latest_date.get(code, 0)
-            if latest_date != 0:
-                # 최신글 부터 DB에 저장된 날짜까지 다 크롤링 한 경우, 중단!
-                # 단, 아래 코드가 정상적으로 작동하려면
-                # min(all fetched posts' date) <= min(this page's posts' date) 이어야 함.
-                if min(pd.to_datetime(posts['date'])) < latest_date:
-                    event.set()
+            db_latest_nid = self.db.latest_nid.get(code, 0)
+            # 최신글 부터 DB에 저장된 날짜까지 다 크롤링 한 경우, 중단!
+            # 단, 아래 코드가 정상적으로 작동하려면
+            # min(all fetched posts' nid) <= min(this page's posts' nid) 이어야 함.
+            if min(posts['nid']) < db_latest_nid:
+                event.set()
 
             return posts
 
@@ -132,26 +131,33 @@ class Crawler:
 
         df = pd.concat(list(map(pd.DataFrame, posts_list)))
         df.date = pd.to_datetime(df.date)
-        df['index'] = range(len(df) - 1, -1, -1)
-        df.sort_values(by=['date', 'index'], inplace=True)
-        del df['index']
-        df.set_index('date', inplace=True)
+        df.sort_values(by='nid', inplace=True)
+        df.set_index('nid', inplace=True)
         df['opinion'].replace('의견없음', 0, inplace=True)
 
         print('\r' + code + ': Done.', end=' ')
         return df
 
     def is_up_to_date(self, code):
+        """
+        종목 토톤실의 가장 최근 글의 날짜와 DB에 저장된 가장 최근 글의 날짜를 비교하여,
+        DB가 최신인지 아닌지 여부를 반환함.
+        (nid를 비교하는 것이 더욱 정확하지만 date로 비교해도 문제는 없다.)
+
+        주의!: 최신글이 답변글인 경우, 글이 게시판 최상단에 위치하지 않아 True가 반환된다.
+        """
         req = requests.get(BASE_URL + '/item/board.nhn?code=' + code)
         page_soup = BeautifulSoup(req.text, 'lxml')
-        latest_post_date = page_soup.select_one('tbody > tr:nth-of-type(3) > td:nth-of-type(1) > span')
-        latest_post_date = pd.to_datetime(latest_post_date.text)
-        if self.db.latest_date.get(code, 0) == latest_post_date:
-            return True
-        else:
-            # TODO: DB에 저장된 데이터가 더 최신인 경우에 대해 처리 필요..(마지막 크롤 이후에 해당 글 삭제된 경우에 발생됨)
-            # TODO: 이 부분에서 거를 수 없는 경우도 존재함
+        web_latest_date = page_soup.select_one('tbody > tr:nth-of-type(3) > td:nth-of-type(1) > span')
+        web_latest_date = pd.to_datetime(web_latest_date.text)
+
+        db_latest_date = self.db.latest_date.get(code, 0)
+        if db_latest_date == 0:
             return False
+        elif db_latest_date < web_latest_date:
+            return False
+        else:
+            return True
 
     def fetch_one(self, code):
         print(code, end=' ')
@@ -160,7 +166,7 @@ class Crawler:
         else:
             t = time.time()
             df = self.fetch_by_code(code)
-            print('({:.2f}sec)        '.format(time.time() - t))
+            print('({:.2f}sec)                 '.format(time.time() - t))
             self.db.write(code, df)
             del df
 
@@ -169,7 +175,7 @@ class Crawler:
         모든 종목 토론실 게시글을 크롤링하는 메소드.
         :output: data/*.db
         """
-        for i, code in enumerate(self.stock_df['종목코드']):
+        for i, code in enumerate(sorted(self.stock_df['종목코드'])):
             print(code, end=': ')
             if self.is_up_to_date(code):
                 print('\r' + code + ': Already up-to-date')
@@ -177,7 +183,7 @@ class Crawler:
             try:
                 t = time.time()
                 df = self.fetch_by_code(code)
-                print('time elaspe={:.2f}s         '.format(time.time() - t))
+                print('({:.2f}sec)                 '.format(time.time() - t))
                 self.db.write(code, df)
                 del df
             except:
--- db_manager.py
+++ db_manager.py
@@ -12,20 +12,45 @@ if not os.path.exists(DB_DIR):
 
 class DB_manager:
     def __init__(self):
-        self.latest_date = self.get_latest_date_df()  # k: code, v: DB에 있는 최근 날짜
+        self.latest_nid = self.get_latest_nid()    # k: code, v: DB에 있는 최근 nid(게시글 고유id)
+        self.latest_date = self.get_latest_date()  # k: code, v: DB에 있는 최근 date
 
     def write(self, code, df):
-        latest_date = self.latest_date.get(code, 0)
-        if latest_date != 0:
+        """
+        :param code: 종목코드
+        :param df: index가 nid인 df
+        """
+        latest_nid = self.latest_nid.get(code, 0)
+        if latest_nid != 0:
             # 이미 DB에 있는 데이터 제외
-            df = df.loc[latest_date:]
-            df = df.iloc[1:]
+            df = df.loc[latest_nid:]
+            from_1 = int(latest_nid in df.index)  # DB의 latest_nid가 받아온 데이터에는 있는지 여부.
+            df = df.iloc[from_1:]
 
         with sqlite3.connect(DB_PATH) as con:
             df.to_sql(code, con, if_exists='append')
 
     @staticmethod
-    def get_latest_date_df():
+    def get_latest_nid():
+        """
+        DB에 저장된 종목들의 가장 최신 글의 nid를 가져오는 메소드.
+        :return: dict {k: code, v: latest posted date}
+        """
+        with sqlite3.connect(DB_PATH) as con:
+            cursor = con.cursor()
+            cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+            db_code_list = cursor.fetchall()
+            db_code_list = [code[0] for code in db_code_list]
+
+            latest_nid = {}
+            for code in db_code_list:
+                cursor.execute("SELECT nid FROM '{}' ORDER BY nid DESC LIMIT 1".format(code))
+                latest_nid[code] = cursor.fetchall()[0][0]
+
+            return latest_nid
+
+    @staticmethod
+    def get_latest_date():
         """
         DB에 저장된 종목들의 가장 최신 글의 날짜를 가져오는 메소드.
         :return: dict {k: code, v: latest posted date}
